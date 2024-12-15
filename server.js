8f596ed8d5b09342a15e3f9f31a665eeaa65f21e
Ottomann <ott.ottender@gmail.com>
2024-12-15 15:51:43 +0200

    week1

---
+++ server.js
@@ -0,0 +1,10 @@
+let firstName = 'Otto';
+let lastName = "Mann";
+
+console.log("Hello, "  + firstName + "!");
+
+//template literals
+console.log(`Hello, ${firstName}!`);
+
+let greeting = `Hello, ${firstName} ${lastName}`;
+console.log(greeting);
